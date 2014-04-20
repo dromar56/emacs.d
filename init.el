d5d0e5f7eda85155b69f8bd7a422159b8e077614
David B <dromar56@gmail.com>
2014-04-21 00:55:50 +0900

    Change theme to zenburn and remove gui/terminal stuff

--- init.el
+++ init.el
@@ -13,28 +13,20 @@
   (mapc 'load (directory-files my-custom-dir 't "^[^#].*el$")))
 
 (setq custom-file (expand-file-name "custom.el" my-custom-dir))
-;; (load custom-file)
 
 (load-theme 'zenburn t)
-;; (load-theme 'solarized-dark t)
+;; (add-to-list 'default-frame-alist '(background-color . "#262626"))
 
-;; (setq my-gui-theme 'solarized-dark)
-;; (setq my-terminal-theme 'solarized-dark)
+(if (daemonp)
+(add-hook 'after-make-frame-functions
+          '(lambda (f)
+             (with-selected-frame f
+               (when (window-system f)
+		 (set-cursor-color "white") ;; Set it to white
+		 ))))
+)
 
-;; (if (daemonp)
-;; (add-hook 'after-make-frame-functions
-;;           '(lambda (f)
-;;              (with-selected-frame f
-;;                (when (window-system f) 
-;; 		 (if (display-graphic-p) 
-;; 		     (load-theme my-gui-theme) 
-;; 		   (load-theme my-terminal-theme))))))
-;; )
-
-;; (if (display-graphic-p) 
-;;     (load-theme my-gui-theme) 
-;;   (load-theme my-terminal-theme))
-
+(set-face-attribute 'default nil :height 100)
 
 (fset 'my-create-header
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([1 67108896 5 134217787 1 11 25 return 25 return 25 up 1 S-right 134217847 up 1 67108896 5 134217848 101 backspace 114 101 103 101 120 114 101 112 108 backspace backspace backspace backspace backspace backspace backspace backspace backspace 114 101 112 108 97 99 101 114 101 return 46 return 25 return 1 11 25 down down 1 11 25 134217849 up 1 C-right C-left] 0 "%d")) arg)))
