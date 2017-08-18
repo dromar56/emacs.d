25df219d2d411460d95b29e3c408b22653981332
David B <dromar56@gmail.com>
2017-08-18 17:21:49 +0200

    ELPA

---
+++ elpa/git-gutter-fringe-20170112.2133/git-gutter-fringe-autoloads.el
@@ -0,0 +1,16 @@
+;;; git-gutter-fringe-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("git-gutter-fringe.el") (22933 47400 663179
+;;;;;;  804000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; git-gutter-fringe-autoloads.el ends here
