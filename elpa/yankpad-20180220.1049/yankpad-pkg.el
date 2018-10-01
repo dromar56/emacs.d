b337f306242487d90078dfeb625eb1d4b6898259
David B <dromar56@gmail.com>
2018-10-01 12:05:28 +0100

    Updated elpa and misc

--- elpa/yankpad-20180220.1049/yankpad-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "yankpad" "20180220.1049" "Paste snippets from an org-mode file" '((emacs "24")) :commit "de672dab9df4a0bffb44cdadaace93d00d9a8848" :url "http://github.com/Kungsgeten/yankpad" :keywords '("abbrev" "convenience"))
---
+++ elpa/yankpad-20180415.350/yankpad-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "yankpad" "20180415.350" "Paste snippets from an org-mode file" '((emacs "24")) :commit "a804f145d17b4b68490ee62ba9a9c4fc7faad130" :keywords '("abbrev" "convenience") :url "http://github.com/Kungsgeten/yankpad")
