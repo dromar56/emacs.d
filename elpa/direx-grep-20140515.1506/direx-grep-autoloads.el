b6c0a55ccf6d42280c270603a72ef144072ef8fe
David B <dromar56@gmail.com>
2018-10-10 10:31:15 +0100

    All packages updated

---
+++ elpa/direx-grep-20140515.1506/direx-grep-autoloads.el
@@ -0,0 +1,15 @@
+;;; direx-grep-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("direx-grep.el") (23485 50739 801465 847000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; direx-grep-autoloads.el ends here
